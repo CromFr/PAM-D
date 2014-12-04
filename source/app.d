76ba36abec33f732ed5dccecbfeb54e39c0231d8
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-12-04 17:09:29 +0100

    Login

--- source/app.d
+++ source/app.d
@@ -5,8 +5,26 @@ import std.string;
 import pam;
 
 
+void setUser(in string login)
+{
+	import core.sys.posix.unistd;
+	import core.sys.posix.pwd;
+	import core.sys.posix.grp;
 
-void pamAuthenticate(in string login, in string passwd)
+	auto pw = getpwnam(login.toStringz);
+
+	if (pw.pw_gid >= 0) {
+		assert(getgrgid(pw.pw_gid) !is null, "Invalid group id!");
+		assert(setegid(pw.pw_gid) == 0, "Error setting group id!");
+	}
+	//if( initgroups(const char *user, gid_t group);
+	if (pw.pw_uid >= 0) {
+		assert(getpwuid(pw.pw_uid) !is null, "Invalid user id!");
+		assert(seteuid(pw.pw_uid) == 0, "Error setting user id!");
+	}
+}
+
+void Login(in string login, in string passwd)
 {
 	static __gshared pam_response* convreply;
 
@@ -44,14 +62,26 @@ void pamAuthenticate(in string login, in string passwd)
 
 	//Start authentification
 	result = pam_authenticate(pamh, 0);
-
-	//End conversation
-	pam_end(pamh, result);
-
 	if (result != pam_return.PAM_SUCCESS) {
 		throw new Exception((cast(pam_return)result).to!string~": Bad password");
 	}
+
+	//result = pam_open_session(pamh, 0);
+	//if (result != pam_return.PAM_SUCCESS) {
+	//	throw new Exception((cast(pam_return)result).to!string~": Could not open session");
+	//}
+
+	setUser(login);
+
+	//End conversation
+	pam_end(pamh, result);
 }
+
+void printUID(){
+	import core.sys.posix.unistd;
+	writeln("uid=",getuid(),"\tgid=",getgid(),"\teuid=",geteuid(),"\tegid=",getegid());
+}
+
 
 void main(string[] args)
 {
@@ -59,7 +89,12 @@ void main(string[] args)
 
 	assert(args.length==3, "Usage: pam-d user password");
 
-	pamAuthenticate(args[1], args[2]);
+	printUID();
+	Login(args[1], args[2]);
+	printUID();
 
-	getuid
+	import std.file;
+	foreach(f ; dirEntries("/root", SpanMode.shallow))
+		writeln(f.name);
+
 }
